87125dfde34260137b340229e12b23c84457e409
Rygulski Martin <147497442+LoonelyyyNardo@users.noreply.github.com>
2025-09-25 12:38:28 +0200

    oprava

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,8 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Main::index1');
-$routes->get('index2', 'Main::index2');
-$routes->get('login', 'Auth::login');
-$routes->post('login', 'Auth::doLogin');
-$routes->get('logout', 'Auth::logout');
-$routes->get('reziseri', 'Reziseri::index');
--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -3,7 +3,6 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use App\Models\Film;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Main extends BaseController
@@ -11,65 +10,5 @@ class Main extends BaseController
     public function index1()
     {
         echo view('index1');
-    }
-
-    public function index2()
-{
-    $filmModel = new \App\Models\Film();
-    $zanrModel = new \App\Models\Zanr();
-    $reziserModel = new \App\Models\Reziser();
-    $herecModel = new \App\Models\Herec();
-
-    $filmy = $filmModel
-        ->select('film.*, reziser.jmeno AS reziser_jmeno, reziser.prijmeni AS reziser_prijmeni')
-        ->join('reziser', 'reziser.id = film.reziser_id', 'left')
-        ->findAll();
-
-    $herci = $herecModel->findAll();
-    foreach ($filmy as &$film) {
-        $nahodnyHerec = $herci[array_rand($herci)];
-        $film->herec_jmeno = $nahodnyHerec->jmeno;
-        $film->herec_prijmeni = $nahodnyHerec->prijmeni;
-    }
-
-    $zanry = $zanrModel->findAll();
-
-    return view('index2', [
-        'filmy' => $filmy,
-        'zanry' => $zanry
-    ]);
-}
-    public function login()
-    {
-        return view('auth/login');
-    }
-
-    public function doLogin()
-    {
-        $session = session();
-        $model = new UzivatelModel();
-
-        $email = $this->request->getPost('email');
-        $heslo = $this->request->getPost('heslo');
-
-        $uzivatel = $model->where('email', $email)->first();
-
-        if ($uzivatel && password_verify($heslo, $uzivatel['heslo'])) {
-            $session->set([
-                'uzivatel_id' => $uzivatel['id'],
-                'uzivatel_jmeno' => $uzivatel['jmeno'],
-                'prihlasen' => true
-            ]);
-            return redirect()->to('/'); // nebo na dashboard
-        } else {
-            $session->setFlashdata('chyba', 'Neplatný email nebo heslo');
-            return redirect()->to('/login');
-        }
-    }
-
-    public function logout()
-    {
-        session()->destroy();
-        return redirect()->to('/');
     }
 }
